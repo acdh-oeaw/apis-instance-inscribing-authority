a60661cc3eefc29746639344b3b350fe555e8cdb
Gytha Ogg <gythaoggscat@gmail.com>
2025-11-07 12:52:31 +0100

    fix: add apps to INSTALLED_APPS
    django_interval and docs

--- apis_ontology/settings.py
+++ apis_ontology/settings.py
@@ -3,4 +3,8 @@ from apis_acdhch_default_settings.settings import *
 ROOT_URLCONF = "apis_ontology.urls"
 ADDITIONAL_APPS = [
     "django_interval",
+    "apis_core.documentation",
 ]
+for app in ADDITIONAL_APPS:
+    if app not in INSTALLED_APPS:
+        INSTALLED_APPS.append(app)
