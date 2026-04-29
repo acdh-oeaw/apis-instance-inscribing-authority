6a643f92b1adee8e74f6996db8e3b21cdff338aa
Saranya Balasubramanian <saranya.balasubramanian@oeaw.ac.at>
2026-04-29 08:16:18 +0200

    fix(entities):preserve html formatting in text fields

--- apis_ontology/templatetags/custom_filters.py
+++ apis_ontology/templatetags/custom_filters.py
@@ -2,12 +2,38 @@
 from django import template
 from django.template.defaultfilters import urlize
 from django.utils.safestring import mark_safe
+from html.parser import HTMLParser
 
 register = template.Library()
 
+class URLizingParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+        self.in_tag = False
+
+    def handle_starttag(self, tag, attrs):
+        self.result.append(self.get_starttag_text())
+        self.in_tag = True
+
+    def handle_endtag(self, tag):
+        self.result.append(f'</{tag}>')
+        self.in_tag = False
+
+    def handle_data(self, data):
+        if not self.in_tag:
+            # Urlize only the data (text content)
+            self.result.append(urlize(data))
+        else:
+            self.result.append(data)
 
 @register.filter
 def urlize_newtab(value):
-    result = urlize(value)
-    result = result.replace("<a ", '<a target="_blank" rel="noopener noreferrer" ')
+    if not value:
+        return value
+    parser = URLizingParser()
+    parser.feed(str(value))
+    result = ''.join(parser.result)
+    # Now modify links to open in new tab
+    result = result.replace('<a ', '<a target="_blank" rel="noopener noreferrer" ')
     return mark_safe(result)
